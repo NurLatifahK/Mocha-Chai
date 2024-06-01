a176e63532f1f756abe79d79b3a54d8eb3595797
Tifa <76276151+NurLatifahK@users.noreply.github.com>
2024-06-01 13:03:33 +0700

    Update rumus.spec.js

--- rumus.spec.js
+++ rumus.spec.js
@@ -1,25 +1,29 @@
 import { expect } from "chai";
-import Rumus from "./rumus.js";
+import HiitungVolumeBola from "./rumus.js";
 
-let RumusBola
 describe('Testing rumus bola', function(){
     // Positive
     it('Hitung volume bola dengan parameter valid', function(){
-        const volume = Rumus.HitungVolume(30)
+        //const volume = Rumus.HitungVolume(30)
+        const r = 30
+        const result = HiitungVolumeBola(r)
 
-        expect(volume).to.equal(113.040)
+        expect(result).to.equal(113097.33552923254)
     })
 
     // Negative
     it('Hitung volume bola dengan jari jari kosong', function(){
-        const volume = {}
+        const r = null
+        const hasil = HiitungVolumeBola(r)
 
-        expect(volume).to.equal('jari jari tidak boleh kosong')
+        expect(hasil).to.equal('Jari jari tidak boleh kosong')
     })
 
     it('Hitung volume bola dengan jari jari string', function(){
-        const volume = Rumus.HitungVolume('Tiga Puluh')
+        //const volume = Rumus.HitungVolume('Tiga Puluh')
+        const r = 'Tiga Puluh'
+        const hasil = HiitungVolumeBola(r)
 
-        expect(volume).to.equal('jari jari tidak boleh bertipe data string')
+        expect(hasil).to.equal('Jari jari harus bertipe data angka')
     })
 })
